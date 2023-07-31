6ae1e41530e81abeb61206c67ececf3d21a7da71
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-07-31 18:14:23 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 61.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int = 0
+        
+        if(num1 == num2) {
+            return 1
+        }else {
+            return -1
+        }
+    }
+}
