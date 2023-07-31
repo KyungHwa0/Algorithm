83adbef79d3e689a4a84dae5aab1c9cb206bd1ca
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-07-31 18:29:25 +0900

    다른사람풀이 추가

--- 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.kt
+++ 프로그래머스/lv0/120803. 두 수의 차/두 수의 차.kt
@@ -1,6 +1,21 @@
 class Solution {
-    fun solution(num1: Int, num2: Int): Int {
-        var answer: Int = 0
-        return num1 - num2
+    // fun solution(num1: Int, num2: Int): Int {
+    //     var answer: Int = 0
+    //     return num1 - num2
+    // }
+
+    // 다른사람풀이1
+    // fun solution(num1: Int, num2: Int) = num1 - num2
+
+    // 다른사람풀이2
+    fun main(args:Array<String>){
+        val num1: Int = readLine()!!.toInt()
+        val num2: Int = readLine()!!.toInt()
+        solution(num1, num2)
     }
+    fun solution(num1: Int, num2: Int): Int {
+        var answer = num1 - num2
+        return answer
+    }
+    
 }
