7fab7f0ebd2be08fc0b6f82880123e1d3ae6d858
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-07-31 18:46:05 +0900

    다른사람코드 내용추가

--- 프로그래머스/lv0/120830. 양꼬치/양꼬치.kt
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.kt
@@ -1,11 +1,14 @@
 class Solution {
-    fun solution(n: Int, k: Int): Int {
-        val freeK = n / 10
-        var answer = n * 12000
+    // fun solution(n: Int, k: Int): Int {
+    //     val freeK = n / 10
+    //     var answer = n * 12000
         
-        if (k > freeK) {
-            answer += (k - freeK) * 2000    // answer = answer + (k-freeK)*2000
-        }
-        return answer
-    }
+    //     if (k > freeK) {
+    //         answer += (k - freeK) * 2000    // answer = answer + (k-freeK)*2000
+    //     }
+    //     return answer
+    // }
+
+    // 다른사람코드1
+    fun solution(n: Int, k: Int) = n * 12000 + k * 2000 - ((n / 10) * 2000)
 }
