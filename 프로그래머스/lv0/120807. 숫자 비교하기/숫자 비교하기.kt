c47dd4e1cc0e90f6e4b285d9bd258af71d76beaf
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-07-31 18:18:12 +0900

    다른사람 코드내용 추가

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.kt
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.kt
@@ -1,11 +1,20 @@
 class Solution {
-    fun solution(num1: Int, num2: Int): Int {
-        var answer: Int = 0
+    // fun solution(num1: Int, num2: Int): Int {
+    //     var answer: Int = 0
         
-        if(num1 == num2) {
-            return 1
-        }else {
-            return -1
-        }
+    //     if(num1 == num2) {
+    //         return 1
+    //     }else {
+    //         return -1
+    //     }
+    // }
+
+    // 다른사람 코드1
+    // fun solution(num1: Int, num2: Int) = if(num1==num2) 1 else -1
+
+    // 다른사람 코드2
+    fun solution(num1: Int, num2: Int): Int = when(num1==num2){
+        true -> 1
+        false -> -1
     }
 }
