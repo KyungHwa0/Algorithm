f57ce8d34add7fbbe568805807bd8432ff22ccd3
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-01 12:04:15 +0900

    다른사람코드 내용추가

--- 프로그래머스/lv0/120820. 나이 출력/나이 출력.kt
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.kt
@@ -1,6 +1,9 @@
 class Solution {
-    fun solution(age: Int): Int {
-        var year = 2022
-        return year - age + 1
-    }
+    // fun solution(age: Int): Int {
+    //     var year = 2022
+    //     return year - age + 1
+    // }
+    
+    // 다른사람 코드
+        fun solution(age: Int) = 2023 - age
 }
