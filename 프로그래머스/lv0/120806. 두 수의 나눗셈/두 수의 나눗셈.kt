bc018439cf273b68dcf2b394b0d5d14ff136617b
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-02 10:33:50 +0900

    다른사람코드 내용추가

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.kt
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.kt
@@ -1,5 +1,8 @@
 class Solution {
-    fun solution(num1: Int, num2: Int): Int {
-        return ((num1.toDouble() / num2) * 1000).toInt()
-    }
+    // fun solution(num1: Int, num2: Int): Int {
+    //     return ((num1.toDouble() / num2) * 1000).toInt()
+    // }
+
+    // 다른사람 코드1
+    fun solution(num1: Int, num2: Int) = num1 * 1000 / num2
 }
