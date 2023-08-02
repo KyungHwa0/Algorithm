c389598c57223cac409920fa41af99d447395017
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-02 10:47:57 +0900

    다른사람코드 내용추가

--- 프로그래머스/lv0/120829. 각도기/각도기.kt
+++ 프로그래머스/lv0/120829. 각도기/각도기.kt
@@ -1,21 +1,42 @@
 class Solution {
+    // fun solution(angle: Int): Int {
+    //     when (true) {
+    //         (angle < 90) -> {
+    //             return 1
+    //         }
+
+    //         (angle == 90) -> {
+    //             return 2
+    //         }
+
+    //         (angle == 180) -> {
+    //             return 4
+    //         }
+
+    //         else -> {
+    //             return 3
+    //         }
+    //     }
+    // }
+
+    // 다른사람 코드1
+    // fun solution(angle: Int): Int {
+    //     if(angle < 90 )
+    //         return 1;
+    //     if(angle == 90)
+    //         return 2;
+    //     if(angle < 180)
+    //         return 3;
+    //     return 4;
+    // }
+
+    // 다른사람 코드2
     fun solution(angle: Int): Int {
-        when (true) {
-            (angle < 90) -> {
-                return 1
-            }
-
-            (angle == 90) -> {
-                return 2
-            }
-
-            (angle == 180) -> {
-                return 4
-            }
-
-            else -> {
-                return 3
-            }
+        return when {
+            angle < 90 -> 1
+            angle == 90 -> 2
+            angle < 180 -> 3
+            else -> 4
         }
     }
 }
