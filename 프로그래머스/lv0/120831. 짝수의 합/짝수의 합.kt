77259f1a2df63f5d83927da331ba192d04791c54
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-03 11:21:50 +0900

    다른사람풀이

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.kt
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.kt
@@ -1,9 +1,12 @@
 class Solution {
-    fun solution(n: Int): Int {
-        var answer = 0
-        for(i in 0..n step 2) {
-            answer += i
-        }
-        return answer
-    }
+    // fun solution(n: Int): Int {
+    //     var answer = 0
+    //     for(i in 0..n step 2) {
+    //         answer += i
+    //     }
+    //     return answer
+    // }
+
+    // 다른사람풀이
+    fun solution(n: Int) = (0..n).filter { it % 2 == 0 }.sum()
 }
