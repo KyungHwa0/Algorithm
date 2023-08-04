fe69fa1d30e0f9c6f0c75798f99e786d0df8c91c
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-04 11:07:27 +0900

    다른사람코드

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.kt
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.kt
@@ -1,14 +1,17 @@
 class Solution {
-    fun solution(numbers: IntArray): IntArray {
-        var answer: IntArray = intArrayOf()
-        var n: Int = 0        
-        answer = numbers
+    // fun solution(numbers: IntArray): IntArray {
+    //     var answer: IntArray = intArrayOf()
+    //     var n: Int = 0        
+    //     answer = numbers
         
-        for(a in numbers) {
-            answer.set(n, a*2)
-            n++
-        }
+    //     for(a in numbers) {
+    //         answer.set(n, a*2)
+    //         n++
+    //     }
         
-        return answer
-    }
+    //     return answer
+    // }
+
+    // 다른사람 코드
+    fun solution(numbers: IntArray) = numbers.average()
 }
