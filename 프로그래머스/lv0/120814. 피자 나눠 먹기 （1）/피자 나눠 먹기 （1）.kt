c2c259f41b287edbb27bc9287a1f25c4ebba4ca3
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-04 11:25:51 +0900

    다른사람코드 추가

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.kt
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.kt
@@ -1,5 +1,22 @@
+// class Solution {
+//     fun solution(n: Int): Int {
+//         return n / 7 + if (n % 7 > 0) 1 else 0
+//     }
+// }
+
+// 다른사람 코드 1
+//Math.ceil 함수를 사용하면 올림연산을 사용할수 있다.
+// import kotlin.math.ceil
+
+// class Solution {
+//     fun solution(n: Int) = ceil(n / 7.0)
+// }
+
+// 다른사람 코드 2
 class Solution {
     fun solution(n: Int): Int {
-        return n / 7 + if (n % 7 > 0) 1 else 0
+        var answer: Int = 0
+        answer=(n+6)/7
+        return answer
     }
 }
