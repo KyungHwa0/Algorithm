59249f8bd6717337ddade36b4a8b19a4805a3148
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-05 18:43:11 +0900

    Update 짝수 홀수 개수.kt

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.kt
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.kt
@@ -1,16 +1,20 @@
 class Solution {
-    fun solution(num_list: IntArray): IntArray {
-        var answer: IntArray = intArrayOf()
-        var even = 0
-        var odd = 0
+    // fun solution(num_list: IntArray): IntArray {
+    //     var answer: IntArray = intArrayOf()
+    //     var even = 0
+    //     var odd = 0
 
-        for (num in num_list) {
-            if (num % 2 == 0) {
-                even++
-            } else {
-                odd++
-            }
-        }
-        return intArrayOf(even, odd)
-    }
+    //     for (num in num_list) {
+    //         if (num % 2 == 0) {
+    //             even++
+    //         } else {
+    //             odd++
+    //         }
+    //     }
+    //     return intArrayOf(even, odd)
+    // }
+
+    // 다른사람 코드 1
+    fun solution(numList: IntArray) = intArrayOf(numList.filter { it % 2 == 0 }.size, numList.filter { it % 2 == 1 }.size)
+    
 }
