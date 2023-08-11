82fb3177cdb63029344ff73d53bf68b7a5de50b8
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-11 12:17:40 +0900

    Update 배열 뒤집기.kt

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.kt
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.kt
@@ -1,5 +1,8 @@
 class Solution {
-    fun solution(num_list: IntArray): IntArray {
-        return num_list.reversed().toIntArray()
-    }
+    // fun solution(num_list: IntArray): IntArray {
+    //     return num_list.reversed().toIntArray()
+    // }
+
+    // 다른사람풀이
+    fun solution(num_list: IntArray) = num_list.reversed()
 }
