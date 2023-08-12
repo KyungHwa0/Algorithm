5cb40c8c06aede728e4161a78148ed101607a855
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-12 18:41:14 +0900

    다른사람풀이 추가

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.kt
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.kt
@@ -1,12 +1,22 @@
 class Solution {
-    fun solution(n: Int): Int {
-        var num = n
-        var sum = 0
+    // fun solution(n: Int): Int {
+    //     var num = n
+    //     var sum = 0
 
-        while (num > 0) {
-            sum += num % 10
-            num /= 10       
+    //     while (num > 0) {
+    //         sum += num % 10
+    //         num /= 10       
+    //     }
+    //     return sum
+    // }
+
+    // 다른사람풀이
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        var temp = n.toString()
+        for(i in temp){
+            answer += i.toString().toInt()
         }
-        return sum
+        return answer
     }
 }
