abd909e37b153cb407eac67c4c755250bbc71024
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-13 13:08:48 +0900

    Update 배열 원소의 길이.kt

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.kt
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.kt
@@ -1,7 +1,13 @@
 class Solution {
-    fun solution(strlist: Array<String>): List<Int> {
-        val lengths = mutableListOf<Int>()
-        strlist.forEach { lengths.add(it.length) }
-        return lengths
-    }
+    // fun solution(strlist: Array<String>): List<Int> {
+    //     val lengths = mutableListOf<Int>()
+    //     strlist.forEach { lengths.add(it.length) }
+    //     return lengths
+    // }
+
+    // 다른사람 풀이1
+    // fun solution(strList: Array<String>) = strList.map { it.length }.toIntArray()
+
+    // 다른사람 풀이2
+    fun solution(strlist: Array<String>): IntArray = IntArray(strlist.size) { strlist[it].length }
 }
