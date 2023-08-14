6c4f1531de12235e0e0434d68966bb8890e090f4
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-14 11:28:45 +0900

    Update 문자열 뒤집기.kt

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.kt
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.kt
@@ -1,6 +1,9 @@
 class Solution {
-    fun solution(my_string: String): String {
-        var answer: String = ""
-        return my_string.reversed()
-    }
+    // fun solution(my_string: String): String {
+    //     var answer: String = ""
+    //     return my_string.reversed()
+    // }
+
+    // 다른사람풀이
+    fun solution(my_string: String): String = my_string.reversed()
 }
