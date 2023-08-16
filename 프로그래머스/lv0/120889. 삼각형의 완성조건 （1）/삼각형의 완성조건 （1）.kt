51a301814ba0d3a966d9dd6a6f725df257145e5e
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-16 10:37:01 +0900

    다른사람풀이 let

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.kt
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.kt
@@ -1,10 +1,13 @@
 class Solution {
-        fun solution(sides: IntArray): Int {
-            sides.sort()
-            return if(sides[2] < sides[0] + sides[1]) {
-                1
-            }else {
-                2
-            }
-        }
+        // fun solution(sides: IntArray): Int {
+        //     sides.sort()
+        //     return if(sides[2] < sides[0] + sides[1]) {
+        //         1
+        //     }else {
+        //         2
+        //     }
+        // }
+
+        // 다른사람 풀이1 (let 알아보기)
+        fun solution(sides: IntArray) = sides.sorted().let { (x, y, z) -> if (x + y > z) 1 else 2 }
     }
