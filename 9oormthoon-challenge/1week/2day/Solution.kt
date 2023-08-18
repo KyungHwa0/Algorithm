5cf771812e56fc3d93c306b46d5210b332646de9
kyunghwa <snoopn303@gmail.com>
2023-08-18 09:07:57 +0900

    1day 2day 내용 복구

--- 9oormthoon-challenge/1week/2day/Solution.kt
+++ 9oormthoon-challenge/1week/2day/Solution.kt
@@ -1,18 +1,23 @@
 fun main(args: Array<String>) {
     // 기능의 수
     val N = readLine()!!.toInt()
-    // 기능별 개발 시간
-    val timeInput = readLine()!!.split(" ")
-    var total = 0
-    for(i in 0 until N) {
-        total += timeInput[i].toInt()
-    }
 
     // 시작시간
-    val T = readLine()!!.toInt()
+    val inputTime = readLine()!!.split(" ")
+    val hour = inputTime[0].toInt()
+    val min = inputTime[1].toInt()
+
+    // 기능별 개발 시간을 한 줄씩 입력받기
+    var total = 0
+    for(i in 0 until N) {
+        val time = readLine()!!.toInt()
+        total += time
+    }
+
     // 완료시간 계산
-    var hour = T + total / 60
-    var min = total % 60
+    var sumHour = hour + (min + total) / 60
+    var sumMin = (min + total) % 60
+
     //결과 출력
-    println("{$hour % 24} ${min}")
+    println("${sumHour % 24} $sumMin")
 }
