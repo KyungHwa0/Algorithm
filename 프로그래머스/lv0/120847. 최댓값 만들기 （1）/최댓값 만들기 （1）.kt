ab9f39d03550d300c9b4177fe20b6724e7c76c26
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-27 01:26:53 +0900

    Update 최댓값 만들기 （1）.kt

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.kt
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.kt
@@ -1,6 +1,7 @@
 class Solution {
     fun solution(numbers: IntArray): Int {
         // 내림차순으로 정렬
+        // sortedDescending() 주어진 컬렉션(리스트나 배열 등)의 원소들을 내림차순으로 정렬한 새로운 리스트를 반환하는 함수
         val sortNumbers = numbers.sortedDescending()
         
         // 첫번째, 두번째 두 수를 곱한다
