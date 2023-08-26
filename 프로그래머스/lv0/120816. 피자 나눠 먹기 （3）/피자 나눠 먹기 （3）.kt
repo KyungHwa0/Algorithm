3a578a488fc3ead3b37bfe40ff9558340452afa3
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-08-26 16:42:06 +0900

    Update 피자 나눠 먹기 （3）.kt

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.kt
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.kt
@@ -1,7 +1,13 @@
 import kotlin.math.ceil
 
 class Solution {
-    fun solution(slice: Int, n: Int): Int {
-        return ceil(n.toDouble() / slice).toInt()
-    }
+    // fun solution(slice: Int, n: Int): Int {
+    //     return ceil(n.toDouble() / slice).toInt()
+    // }
+
+    // 다른사람 코드
+    // fun solution(slice: Int, n: Int) = n / slice + if (n % slice > 0) 1 else 0
+
+    // 다른사람코드
+    fun solution(slice: Int, n: Int) = ceil(n / slice.toDouble())
 }
