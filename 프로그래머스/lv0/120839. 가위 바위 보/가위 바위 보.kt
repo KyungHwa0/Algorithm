68fd2436e3f401a049a6c5fb49029f0b2a8e0289
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-09-01 18:32:18 +0900

    Update 가위 바위 보.kt

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.kt
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.kt
@@ -1,16 +1,25 @@
 class Solution {
-    fun solution(rsp: String): String {
-        var answer: String = ""
-        for (r in rsp) {
-            when (r) {
-                // 가위는 바위에 짐, 바위 추가
-                '2' -> answer += '0'
-                // 바위는 보에 짐, 보 추가
-                '0' -> answer += '5'
-                // 보는 가위에 짐, 가위 추가
-                '5' -> answer += '2'
-            }
+    // fun solution(rsp: String): String {
+    //     var answer: String = ""
+    //     for (r in rsp) {
+    //         when (r) {
+    //             // 가위는 바위에 짐, 바위 추가
+    //             '2' -> answer += '0'
+    //             // 바위는 보에 짐, 보 추가
+    //             '0' -> answer += '5'
+    //             // 보는 가위에 짐, 가위 추가
+    //             '5' -> answer += '2'
+    //         }
+    //     }
+    //     return answer
+    // }
+
+    // 다른사람풀이
+    fun solution(rsp: String): String  = rsp.map{ 
+        when(it){
+            '2' -> '0'
+            '0' -> '5'
+            else -> '2'
         }
-        return answer
-    }
+    }.joinToString("")
 }
