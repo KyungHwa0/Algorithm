828ddb49e574bb24ec3954ade93903409b33944a
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2023-12-26 16:49:10 +0900

    Update 홀수.kt

--- 백준/Bronze/2576. 홀수/홀수.kt
+++ 백준/Bronze/2576. 홀수/홀수.kt
@@ -1,32 +1,53 @@
-fun main () {
-    val numbers = mutableListOf<Int>()
+// fun main () {
+//     val numbers = mutableListOf<Int>()
 
-    // 7개의 숫자를 한 줄씩 입력받음
-    for (i in 1..7) {
-        while (true) {
-            val number = readLine()!!.toInt()
+//     // 7개의 숫자를 한 줄씩 입력받음
+//     for (i in 1..7) {
+//         while (true) {
+//             val number = readLine()!!.toInt()
 
-            if (number < 100) {
-                numbers.add(number)
-                break
-            } else {
-                println("100보다 작은 자연수를 다시 입력 하세요")
-            }
+//             if (number < 100) {
+//                 numbers.add(number)
+//                 break
+//             } else {
+//                 println("100보다 작은 자연수를 다시 입력 하세요")
+//             }
+//         }
+//     }
+
+
+//     val oddNumbers = numbers.filter { it % 2 != 0 }
+
+//     if(oddNumbers.isEmpty()) {
+//         println("-1")
+//         return
+//     }
+
+//     val sum = oddNumbers.sum()
+//     // 최소값 찾기 : minOrNull
+//     val minOdd = oddNumbers.minOrNull()
+
+//     println(sum)
+//     println(minOdd)
+// }
+
+// 다른사람 코드
+fun main() {
+    var min = Int.MAX_VALUE
+
+    var sum = 0
+
+    repeat(7) {
+        val n = readln().toInt()
+        if(n % 2 == 1) {
+            sum += n
+            min = min(min,n)
         }
     }
 
-
-    val oddNumbers = numbers.filter { it % 2 != 0 }
-
-    if(oddNumbers.isEmpty()) {
-        println("-1")
-        return
+    if(sum == 0) {
+        println(-1)
+    } else {
+        println("$sum\n$min")
     }
-
-    val sum = oddNumbers.sum()
-    // 최소값 찾기 : minOrNull
-    val minOdd = oddNumbers.minOrNull()
-
-    println(sum)
-    println(minOdd)
 }
