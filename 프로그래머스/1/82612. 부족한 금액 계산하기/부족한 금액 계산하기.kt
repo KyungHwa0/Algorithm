d455567e6551213b4d31476d1c587191e6c252e7
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2024-01-28 23:24:02 +0900

    Update 부족한 금액 계산하기.kt

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.kt
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.kt
@@ -1,8 +1,11 @@
 class Solution {
-    fun solution(price: Int, money: Int, count: Int): Long {
-        val totalCost = price.toLong() * count * (count + 1) / 2
-        val shortage = totalCost - money
+    // fun solution(price: Int, money: Int, count: Int): Long {
+        // val totalCost = price.toLong() * count * (count + 1) / 2
+        // val shortage = totalCost - money
 
-        return if (shortage > 0) shortage else 0
+        // return if (shortage > 0) shortage else 0
+
+    // 다른사람 풀이
+    fun solution(price: Int, money: Int, count: Int): Long = (1..count).map { it * price.toLong() }.sum().let { if (money > it) 0 else it - money }
     }
 }
