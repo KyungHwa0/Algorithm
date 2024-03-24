cac41b96575080ae063cec3e20f0d39dcb5740de
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2024-03-24 16:19:55 +0900

    Update 가장 큰 수.kt

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.kt
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.kt
@@ -5,3 +5,15 @@ class Solution {
         return if (sortedNumbers[0] == "0") "0" else sortedNumbers.joinToString("")
     }
 }
+
+
+// class Solution {
+//     fun solution(numbers: IntArray): String {
+//         var answer = ""        
+//         numbers.sortedWith(Comparator({num1: Int, num2: Int -> "$num2$num1".compareTo("$num1$num2")})).forEach { answer += it }
+//         if ("(0*)".toRegex().replace(answer, "").isEmpty()) {
+//             answer = "0"
+//         }
+//         return answer
+//     }
+// }
