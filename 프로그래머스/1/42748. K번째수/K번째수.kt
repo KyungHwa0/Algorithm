3283c238d934d2333cea975a013b738c29e2b4cf
KyungHwa <124041716+KyungHwa0@users.noreply.github.com>
2024-05-18 22:28:00 +0900

    [level 1] Title: K번째수, Time: 22.48 ms, Memory: 64.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.kt
+++ 프로그래머스/1/42748. K번째수/K번째수.kt
@@ -1,8 +1,8 @@
 class Solution {
     fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
-            return commands.map { command ->
-                array.slice(IntRange(command[0] - 1, command[1] - 1)).sorted()[command[2] - 1]
-            }
-                .toIntArray()
-        }
+        return commands.map { command ->
+            val slicedArray = array.slice(IntRange(command[0] - 1, command[1] - 1)).sorted()
+            slicedArray[command[2] - 1]
+        }.toIntArray()
+    }
 }
